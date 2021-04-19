483ffda35a4166c6b39d2b1d36f67bc103b2c6a8
Thiago Matos <tms.42@hotmail.com>
2021-04-19 11:26:21 -0300

    retirada de variaveis não usadas

--- src/Contagem.js
+++ src/Contagem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector, useDispatch} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import Input from './Input';
 import Filter from './Filter';
 import {retornaTotal } from './store/contagem';
